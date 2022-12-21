88260de3c0adf8610a2c1a9b6d1364fa51d5dd9a
BekirK-C <b.b.kamac@gmail.com>
2022-12-21 14:14:20 +0300

    Dummy Data ile Onion mimari işleyişini öğrenme

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -1,3 +1,5 @@
+using ETicaretAPI.Persistence;
+
 namespace ETicaretAPI.API
 {
     public class Program
@@ -6,16 +8,14 @@ namespace ETicaretAPI.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddPersistenceServices();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
