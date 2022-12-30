76b957e5bbf0621b0a8fa861612034406c745fb9
BekirK-C <b.b.kamac@gmail.com>
2022-12-30 10:48:50 +0300

    7. Ef Core üzerinden yapılan sorgulamada gelen dataların track edilip-edilmemesini sağlamak için repository'de gereken optimizasyonun yapılması

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -22,13 +22,17 @@ namespace ETicaretAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new() {Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10},
-                new() {Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 30},
-                new() {Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreatedDate = DateTime.UtcNow, Stock = 50}
-            });
-            var count = await _productWriteRepository.SaveAsync();
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new() {Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10},
+            //    new() {Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 30},
+            //    new() {Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreatedDate = DateTime.UtcNow, Stock = 50}
+            //});
+            //var count = await _productWriteRepository.SaveAsync();
+
+            Product product =  await _productReadRepository.GetByIdAsync("6820ada9-d851-4ffb-b06a-0e8eca859a27", false);
+            product.Name = "Mehmet";
+            await _productWriteRepository.SaveAsync();
         }
         [HttpGet("id")]
         public async Task<IActionResult> Get(string id)
